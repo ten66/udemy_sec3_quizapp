d7faf7fd33cd8d38772c39e92edede34432b7214
ten66 <ikesho.1217@gmail.com>
2023-05-10 16:08:57 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/udemy_sec3_quizapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udemy_sec3_quizapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_sec3_quizapp/start_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
